c99a79e884a39363dc263055d2554c6bed2c5fff
Rival <112675933+Rivalslol@users.noreply.github.com>
2024-03-25 00:40:56 -0400

    Update gatohub.lua

--- gatohub.lua
+++ gatohub.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Catto-YFCN/GatoHub/main/PetCatchers"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Catto-YFCN/GatoHub/main/PetCatchers", "gatohub"))()
