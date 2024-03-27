0ca58b74f2de1ad8ebd0baff94d74b5a3eb801a2
Rival <112675933+Rivalslol@users.noreply.github.com>
2024-03-27 15:34:08 -0400

    Update noti.lua

--- noti.lua
+++ noti.lua
@@ -1,11 +1,11 @@
 local notificationLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/laagginq/ui-libraries/main/xaxas-notification/src.lua"))();
 local notifications = notificationLibrary.new({            
-    NotificationLifetime = 3, 
+    NotificationLifetime = 6, 
     NotificationPosition = "Middle",
     
     TextFont = Enum.Font.Code,
     TextColor = Color3.fromRGB(255, 255, 255),
-    TextSize = 15,
+    TextSize = 20,
     
     TextStrokeTransparency = 0, 
     TextStrokeColor = Color3.fromRGB(0, 0, 0)
