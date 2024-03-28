18eca3d00304a0260689e4e57b847ffe1d2dbd9c
Rival <112675933+Rivalslol@users.noreply.github.com>
2024-03-28 17:38:26 -0400

    Update gatohub.lua

--- gatohub.lua
+++ gatohub.lua
@@ -1,1 +1,18 @@
+local notificationLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/laagginq/ui-libraries/main/xaxas-notification/src.lua"))();
+local notifications = notificationLibrary.new({            
+    NotificationLifetime = 6, 
+    NotificationPosition = "Middle",
+    
+    TextFont = Enum.Font.Code,
+    TextColor = Color3.fromRGB(255, 255, 255),
+    TextSize = 20,
+    
+    TextStrokeTransparency = 0, 
+    TextStrokeColor = Color3.fromRGB(0, 0, 0)
+});
+
+notifications:BuildNotificationUI();
+notifications:Notify("Gato Hub [ LOADED ]");
+
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Catto-YFCN/GatoHub/main/PetCatchers", "gatohub"))()
