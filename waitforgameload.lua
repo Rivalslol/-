c197572d70844f7b3e3359a269d21139f5c35e88
Rival <112675933+Rivalslol@users.noreply.github.com>
2024-04-13 03:30:40 -0400

    Update waitforgameload.lua

--- waitforgameload.lua
+++ waitforgameload.lua
@@ -5,17 +5,14 @@ until game:IsLoaded()
 repeat
     task.wait(1)
 until game.PlaceId ~= nil
-
+print("[CLIENT] Loaded PlaceId")
 repeat
     task.wait(1)
 until game:GetService("Players").LocalPlayer and game:GetService("Players").LocalPlayer.Character and game:GetService("Players").LocalPlayer.Character.HumanoidRootPart
-
+print("[CLIENT] Loaded Players")
 repeat
     task.wait(1)
-until game:GetService("Workspace"):FindFirstChild("Map") or game:GetService("Workspace"):FindFirstChild("Map2")
-
-repeat
+until game:GetService("Workspace")
+print("[CLIENT] Loaded Workspace")
     task.wait(1)
-until game:GetService("Workspace").__THINGS and game:GetService("Workspace").__DEBRIS
-
 print("[CLIENT] Loaded Game")
